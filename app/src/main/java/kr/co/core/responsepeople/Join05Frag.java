582eb4bcb1cbe37d0d6776a32596ec3360ba86ce
choi <puch93@naver.com>
2020-07-19 09:28:02 +0900

    2020-07-19 first commit

---
+++ app/src/main/java/kr/co/core/responsepeople/Join05Frag.java
@@ -0,0 +1,27 @@
+package kr.co.core.responsepeople;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+
+import kr.co.core.responsepeople.databinding.FragmentJoin03Binding;
+
+public class Join05Frag extends BaseFrag {
+    FragmentJoin03Binding binding;
+    Activity act;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_join_03, container,false);
+        act = getActivity();
+
+        return binding.getRoot();
+    }
+}
